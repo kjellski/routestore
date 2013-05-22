d9c630676f80808baab116f75970daca10e25cf0
kjellski <otto.kjell@gmail.com>
2013-05-22 11:13:13 +0200

    added activity as model, now writing tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521164727) do
+ActiveRecord::Schema.define(:version => 20130522090605) do
+
+  create_table "activities", :force => true do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pois", :force => true do |t|
     t.float    "latitude"
